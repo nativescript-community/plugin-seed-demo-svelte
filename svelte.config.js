99fac5f8907b9571e64d2835e0c491a7c8df856b
Martin Guillon <dev@akylas.fr>
2026-08-17 21:44:00 +0200

    chore: svelte build fxix

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,33 @@
 const sveltePreprocess = require('svelte-preprocess');
 
+// this can be called either through svelte-loader where we want either __ANDROID__ or __IOS__ to be defined but not both
+// or through svelte-check where we want both so everything is checked
+const webpack_env = process.env['NATIVESCRIPT_WEBPACK_ENV']
+    ? JSON.parse(process.env['NATIVESCRIPT_WEBPACK_ENV'])
+    : {
+          production: false,
+          android: true,
+          ios: true
+      };
+
 module.exports = {
     compilerOptions: {
         namespace: 'foreign'
     },
     preprocess: [
         sveltePreprocess({
+            replace: [
+                [/PRODUCTION/g, !!webpack_env.production],
+                [/__ANDROID__/g, !!webpack_env.android],
+                [/__IOS__/g, !!webpack_env.ios]
+            ].concat([]),
             typescript: {
                 compilerOptions: {
-                    target: 'es2019'
+                    verbatimModuleSyntax: true,
+                    target: 'es2020'
                 }
             }
         })
+        // svelteNativePreprocessor()
     ]
 };
